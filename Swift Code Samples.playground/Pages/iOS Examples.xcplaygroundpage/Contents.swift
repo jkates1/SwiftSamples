eaac08f4d25d1d93ba8bfb877936541ee58d0c11
agilecomm <jeremy.kates@charter.net>
2016-01-27 11:21:30 -0600

    1-27-16-3

---
+++ Swift Code Samples.playground/Pages/iOS Examples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+
+import UIKit
+import XCPlayground
+
+class TableViewController: UITableViewController {
+    
+    let tableData = [["3"], ["13"], ["Red","White","Blue"]]
+    
+    override func viewDidLoad() {
+        self.tableView.delegate = self
+        self.tableView.sectionIndexBackgroundColor = UIColor.blackColor()
+    }
+}
+
+extension TableViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData[section].count
+    }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tableData[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Colors"
+        case 1:
+            return "Stripes"
+        case 2:
+            return "Which Colors"
+        default :
+            return "Default"
+        }
+    }
+}
+
+let thisTable = TableViewController()
+thisTable.tableData
+thisTable.view!
+XCPlaygroundPage.currentPage.liveView = thisTable.view
