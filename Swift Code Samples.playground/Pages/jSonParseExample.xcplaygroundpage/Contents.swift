9e8202e410a7515f67358db7a3e35720c179676c
agilecomm <jeremy.kates@charter.net>
2016-01-28 13:25:53 -0600

    1-28-16-1

---
+++ Swift Code Samples.playground/Pages/jSonParseExample.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import XCPlayground
+
+//Parse jSon Data
+//TODO: Build jSonParse Stub
+
+
+class TableViewController1 : UITableViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        self.view.frame = CGRectMake(0, 0, 400, 400)
+      //  let jsonParser = jSonParser.parseJson()
+    
+    }
+}
+
+let tableView1 = TableViewController1()
+XCPlaygroundPage.currentPage.liveView = tableView1.view!
+
+
+
