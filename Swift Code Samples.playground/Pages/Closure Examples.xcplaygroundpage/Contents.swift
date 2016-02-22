6a7be44b5314d6572c6f69e08a5339c3e9a44445
agilecomm <jeremy.kates@charter.net>
2016-02-21 18:43:13 -0600

    More Closure Samples

--- Swift Code Samples.playground/Pages/Closure Examples.xcplaygroundpage/Contents.swift
+++ Swift Code Samples.playground/Pages/Closure Examples.xcplaygroundpage/Contents.swift
@@ -1,19 +1,42 @@
 import Foundation
 
-func closureExample(name: String) -> () -> String {
+
+//Basic Closure Simplicity
+let circumferenceByDiameter  = { $0 * M_PI }
+circumferenceByDiameter(4)
+
+//Add 2 numbers return closure that takes 2 summands arguments
+func add2Numbers() -> (num1 : Int, num2: Int) -> Int {
+    func numbers2Add(first: Int, second: Int) -> Int {
+        return first + second
+    }
+    return numbers2Add
+}
+
+let sum = add2Numbers()
+let sumOf2Nums = sum(num1: 32, num2: 12)
+print(sumOf2Nums)
+
+//Add LastName Example
+func closureExample(lastName: String) -> (firstName: String) -> String {
     
-    func addLastName() -> String {
-        let fullName = name + " Kates"
+    func addLastName(firstName: String) -> String {
+        let fullName = firstName + " Kates"
         return fullName
     }
     
     return addLastName
 }
 
-let functionArg = closureExample("Jeremy")
+let functionAsArgument = closureExample("Jeremy")
 
-let myName = "FullName: " + functionArg()
+let myName = "FullName: " + functionAsArgument(firstName: "Jeremy")
 print(myName)
 
+let listOfFirstNames = ["Jerry", "Jim", "Larry", "Mike", "Brian"]
 
+listOfFirstNames.map {
+    
+    $0 + " Davidson"
+}
 
